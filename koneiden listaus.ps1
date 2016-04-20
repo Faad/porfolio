3307587baa8c0eccc0822d9dd50f797d57249786
Sasu Ölander <sasuolander@yahoo.com>
2016-04-20 10:07:55 +0300

    Added files via upload

---
+++ koneiden listaus.ps1
@@ -0,0 +1,21 @@
+﻿$adconnection = "OU=Asennus, OU=Kauniainen, OU=diak, DC=diak, DC=fi" ## määritetään mistä kysely tehdään
+$adpropertie= @("Name", "Created", "Description", "LastLogonDate", "OperatingSystem") ## määritetään kerättävät arvot
+$suomenkielinen= @{Expression= {$_.Name};Label="Nimi"},                          ## määritetään taulun nimet
+                  @{Expression= {$_.Created};Label="Luotu"},
+                  @{Expression= {$_.Description};Label="Kuvaus"},
+                  @{Expression= {$_.LastLogonDate};Label="Viimeisin kirjautuminen"},
+                  @{Expression= {$_.OperatingSystem};Label="Kayttojarjestelma"}
+ 
+ <#
+
+ Tiedot syötetään Get-ADComputer komentoon. Suodatetaan halutuut muotoon. 
+
+ #>
+                 
+ 
+ Get-ADComputer -Filter * -SearchBase $adconnection -Properties $adpropertie | Select $adpropertie |
+    Select-Object $suomenkielinen | Export-Csv ".\tietokonelista2.csv" -NoTypeInformation
+    
+    ## jos siirtää csv tiedostoon dataa on käytettävä Select-Object
+
+    ##Get-ADComputer -Filter * -SearchBase "OU=Asennus,OU=Kauniainen,OU=diak,DC=diak,DC=fi"
